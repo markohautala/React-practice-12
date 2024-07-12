4cdaebd44f6e212ea2096533b9627054aaf0afdd
markohautala <markohautala@live.se>
2024-07-12 17:48:37 +0200

    ComponentWillUnmount() practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import css from './App.module.css';
 /* import LifeCyclesCDM from './components/LifeCyclesCDM'; */
-import LifeCyclesCDU from './components/LifeCyclesCDU';
+/* import LifeCyclesCDU from './components/LifeCyclesCDU'; */
+import LifeCyclesCWU from './components/LifeCyclesCWU';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       {/* <LifeCyclesCDM /> */}
-      <LifeCyclesCDU />
+      {/*<LifeCyclesCDU /> */}
+      <LifeCyclesCWU />
     </div>
   );
 }
