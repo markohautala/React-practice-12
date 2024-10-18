0f7fb6f847e4ad22135820cc50a5dbbc0e79b8a9
markohautala <markohautala@live.se>
2024-10-18 18:54:30 +0200

    Practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,9 @@
-import css from './App.module.css';
-/* import LifeCyclesCDM from './components/LifeCyclesCDM'; */
-/* import LifeCyclesCDU from './components/LifeCyclesCDU'; */
-import Hooks from './components/Hooks';
+import NestingComponents from "./components/NestingComponents";
 
 function App() {
-  return (
-    <div className={css.App}>
-      {/* Add your components here */}
-      {/* <LifeCyclesCDM /> */}
-      {/*<LifeCyclesCDU /> */}
-      <Hooks />
-    </div>
-  );
+  return <div>
+      <NestingComponents />
+  </div>;
 }
 
 export default App;
