644bda5255841ab02be8329550c7e715316c7091
markohautala <markohautala@live.se>
2024-10-29 07:22:48 +0100

    Add new component with filter and search

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import MethodsAsPropsParent from "./components/ControlledForms";
 import ControlledForms from "./components/ControlledForms";
 import UnControlledForms from "./components/UnControlledForms";
+import SearchComponent from "./components/SearchComponent";
 
 function App() {
   return (
     <div>
-      <UnControlledForms />
+      <SearchComponent />
     </div>
   );
 }
---
+++ my-app/src/components/SearchComponent.jsx
@@ -0,0 +1,36 @@
+// this component should display and be able to search or filter the names.js
+
+import React, { useState } from "react";
+import names from "../names.js";
+
+export default function SearchComponent() {
+
+    const [search, setSearch] = useState("");
+    const [filteredNames, setFilteredNames] = useState(names);
+
+    const handleChange = (e) => {
+        const searchTerm = e.target.value;
+        setSearch(searchTerm);
+        const filteredNames = names.filter((name) =>
+            name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredNames(filteredNames);
+    };
+
+    return (
+        <div>
+            <h2>Search for a name:</h2>
+            <input
+                type="text"
+                placeholder="Search by name"
+                value={search}
+                onChange={handleChange}
+            />
+            <ul>
+                {filteredNames.map((name, index) => (
+                    <li key={index}>{name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
